cbb4be931bb71138a3284b91e10da46785cebe9e
leewoosung808 <21700543@handong.edu>
2021-09-07 12:35:09 +0900

    Create HelloWorld project

---
+++ src/com/example/lab1/Main.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.example.lab1;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!\n");
+		// TODO Auto-generated method stub
+	}
+
+}
